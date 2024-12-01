b7509ae4954f28eb5db3f0d47472ac0670e45571
Sarang <gaikwadsarang111@gmail.com>
2024-12-01 15:19:10 +0530

    Tip Calculator

---
+++ Tip Calculator/script.js
@@ -0,0 +1,13 @@
+function calculateTip() {
+     // get the input type values
+    const billAmount = parseInt(document.getElementById('bill-amount').value) 
+    const tipPercentage = parseInt(document.getElementById('tip-percentage').value) 
+
+    const tipAmount = billAmount * (tipPercentage/100)
+
+    const totalAmount = tipAmount + billAmount
+
+    document.getElementById('tip-amount').textContent = '$' + tipAmount.toFixed(2)
+
+    document.getElementById('total-amount').textContent = '$' + totalAmount.toFixed(2)
+}
